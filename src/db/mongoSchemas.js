57b2e01af5e58a85d2d2ca39a5f1ca4e9e4e0673
Gabriel Rossetto <rossetto.2306@gmail.com>
2021-08-02 00:26:48 -0300

    ajustes deploy heroku

--- src/db/mongoSchemas.js
+++ src/db/mongoSchemas.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/db_login', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(`mongodb+srv://admin:${process.env.MONGOKEY}@cluster0.grv5g.mongodb.net/Cluster0?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const Schema = mongoose.Schema;
 
